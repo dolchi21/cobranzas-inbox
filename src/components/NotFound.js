2f57b898770de723423537c799467e318f470ba9
Axel Dolce <axel.dolce@gmail.com>
2016-07-25 20:40:42 -0300

    NotFound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    
+    constructor (props) { super(props) }
+
+    render () {
+        return (<div>Not Found</div>);
+    }
+
+}
+
+export default NotFound;
