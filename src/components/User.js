ea2774ba5c7f7bac2545f5e28c643a631cb51240
Axel Dolce <axel.dolce@gmail.com>
2016-07-25 17:30:31 -0300

    commit

---
+++ src/components/User.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class User extends React.Component {
+	
+	// eslint-disable-next-line
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		return (
+			<div>
+				<h4>Username</h4>
+				<p>user bio</p>
+			</div>
+			);
+	}
+
+}
+
+export default User;
