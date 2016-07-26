fa5c52b45485ade9ec1556463b5dd3530afee9e8
Axel Dolce <axel.dolce@gmail.com>
2016-07-25 21:41:12 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 
-import router from './router.js';
+import App from './App.js';
+import NotFound from './components/NotFound.js';
+
+var router = (
+    <Router history={hashHistory}>
+        <Route path="/" component={App}></Route>
+        <Route path="*" component={NotFound}/>
+    </Router>
+    );
 
 ReactDOM.render(
-  <router/>,
-  document.getElementById('root')
+    router,
+    document.getElementById('root')
 );
